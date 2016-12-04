dc6a78905f0ec7ba085be4b72c5b9012d484d900
felerminoali <felerminoali@gmail.com>
2016-12-04 15:40:05 +0800

    First commit

--- template/_header.php
+++ template/_header.php
@@ -15,9 +15,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <title>Ecommerce website project</title>
-    <meta name="description" content="Ecommerce website project"/>
-    <meta name="keywords" content="Ecommerce website project"/>
+    <title>Online Public Access Catalog </title>
+    <meta name="description" content="Online Public Access Catalog project"/>
+    <meta name="keywords" content="Online Public Access Catalog project"/>
     <meta http-equiv="imagetoolbar" content="no"/>
     <link href="/css/core.css" rel="stylesheet" type="text/css"/>
 </head>
