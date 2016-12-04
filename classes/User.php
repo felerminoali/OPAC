af51941603d631a316cee94b40d5c88d4150771e
felerminoali <felerminoali@gmail.com>
2016-12-04 19:53:28 +0800

    First commit

--- classes/User.php
+++ classes/User.php
@@ -147,11 +147,11 @@ class User extends Application
 //    }
 
 
-    function randomPassword() {
+    function randomPassword($length = 6) {
         $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
         $pass = array(); //remember to declare $pass as an array
         $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-        for ($i = 0; $i < 8; $i++) {
+        for ($i = 0; $i < $length; $i++) {
             $n = rand(0, $alphaLength);
             $pass[] = $alphabet[$n];
         }
