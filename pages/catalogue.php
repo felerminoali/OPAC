497b3a642cc517f6e7f531e380d9d8b1313d21f4
felerminoali <felerminoali@gmail.com>
2016-12-06 18:09:03 +0800

    First commit

--- pages/catalogue.php
+++ pages/catalogue.php
@@ -8,7 +8,7 @@
 
 Login::restrictFront();
 
-require_once ("_header.php");
+require_once("_header.php");
 ?>
 
 
@@ -30,18 +30,19 @@ require_once ("_header.php");
                                 </label>
                             </td>
                         </tr>
-                        <tr>
-                            <th><label for="library">Library:</label></th>
-                            <td>
 
 
-                                <?php
-
-                                $objLibrary = new Library();
-                                $libraries = $objLibrary->getlibraries();
-                                if (!empty($libraries)) {
+                        <?php
+                        $objLibrary = new Library();
+                        $libraries = $objLibrary->getlibraries();
+                        if (!empty($libraries)) {
+                            ?>
+                            <tr>
+                                <th><label for="library">Library:</label></th>
+                                <td>
+                                    <?php
                                     $out = "<select name=\"library\" id=\"library\" class=\"sel\">";
-                                        $out .= "<option value=\"\">All libraries&hellip;</option>";
+                                    $out .= "<option value=\"\">All libraries&hellip;</option>";
                                     foreach ($libraries as $library) {
                                         $out .= "<option value=\"";
                                         $out .= $library['id'];
@@ -51,147 +52,151 @@ require_once ("_header.php");
                                         $out .= "</option>";
                                     }
                                     $out .= "</select>";
-                                    return $out;
-                                }
+                                    echo $out;
+                                    ?>
+                                </td>
+                            </tr>
+                        <?php } ?>
 
-                                ?>
-
-<!--                                <select>-->
-<!--                                    <option value="volvo">Volvo</option>-->
-<!--                                    <option value="saab">Saab</option>-->
-<!--                                    <option value="mercedes">Mercedes</option>-->
-<!--                                    <option value="audi">Audi</option>-->
-<!--                                </select>-->
-                            </td>
-                        </tr>
                     </table>
 
                     <ul id="filters">
                         <li>
-<!--                            <div class="tickbox">-->
-                                <input type="checkbox" name="YearCheckFilter"
-                                       id=""
-                                       value=""
-                                />
-                                <span> Books </span>
-<!--                            </div>-->
+                            <!--                            <div class="tickbox">-->
+                            <input type="checkbox" name="YearCheckFilter"
+                                   id=""
+                                   value=""
+                            />
+                            <span> Books </span>
+                            <!--                            </div>-->
                         </li>
                         <li>
-<!--                            <div class="tickbox">-->
-                                <input type="checkbox" name="YearCheckFilter"
-                                       id=""
-                                       value=""
-                                />
-                                <span> Audio / Video </span>
-<!--                            </div>-->
+                            <!--                            <div class="tickbox">-->
+                            <input type="checkbox" name="YearCheckFilter"
+                                   id=""
+                                   value=""
+                            />
+                            <span> Audio / Video </span>
+                            <!--                            </div>-->
                         </li>
                         <li>
-<!--                            <div class="tickbox">-->
-                                <input type="checkbox" name="YearCheckFilter"
-                                       id=""
-                                       value=""
-                                />
-                                <span> Periodicals </span>
-<!--                            </div>-->
+                            <!--                            <div class="tickbox">-->
+                            <input type="checkbox" name="YearCheckFilter"
+                                   id=""
+                                   value=""
+                            />
+                            <span> Periodicals </span>
+                            <!--                            </div>-->
                         </li>
                     </ul>
 
                 </form>
-                
-                
-<!--                <p>What is Lorem Ipsum?</p>-->
-<!--                <p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.-->
-<!--                </p>-->
-<!--                <p>Refer to the <a href="https://myopac.herokuapp.com/?page=about">link</a> if you need more info.</p>-->
+
+
+                <!--                <p>What is Lorem Ipsum?</p>-->
+                <!--                <p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.-->
+                <!--                </p>-->
+                <!--                <p>Refer to the <a href="https://myopac.herokuapp.com/?page=about">link</a> if you need more info.</p>-->
             </div>
         </div>
     </div>
 
-<div class="paging">
-    <p>Your search found <strong>18201</strong> records. Displaying records <strong>1</strong> to <strong>10</strong>.</p>
-</div>
+    <div class="paging">
+        <p>Your search found <strong>18201</strong> records. Displaying records <strong>1</strong> to
+            <strong>10</strong>.</p>
+    </div>
 
-<h1></h1>
+    <h1></h1>
 
 
     <div class="catalogue_wrapper">
         <div class="catalogue_wrapper_left">
 
             <a href="/?page=catalogue-item&amp;category=3&amp;id=31">
-                <img src="media/catalogue/unavailable.png" alt="Drum" width="120" />
+                <img src="media/catalogue/unavailable.png" alt="Drum" width="120"/>
             </a>
         </div>
         <div class="catalogue_wrapper_right">
             <h4>
                 <a href="/?page=catalogue-item&amp;category=3&amp;id=31"></a>
-                Drum                        </h4>
+                Drum </h4>
             <h4>
                 Price: &pound145.00;
             </h4>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#039;s standard dummy text ever since&hellip;</p>
-            <p><a href="#" class="add_to_basket" rel="31_1">Add to basket</a> </p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry&#039;s standard dummy text ever since&hellip;</p>
+            <p><a href="#" class="add_to_basket" rel="31_1">Add to basket</a></p>
         </div>
     </div>
     <div class="catalogue_wrapper">
         <div class="catalogue_wrapper_left">
 
             <a href="/?page=catalogue-item&amp;category=3&amp;id=31">
-                <img src="media/catalogue/unavailable.png" alt="Drum" width="120" />
+                <img src="media/catalogue/unavailable.png" alt="Drum" width="120"/>
             </a>
         </div>
         <div class="catalogue_wrapper_right">
             <h4>
                 <a href="/?page=catalogue-item&amp;category=3&amp;id=31"></a>
-                Drum                        </h4>
+                Drum </h4>
             <h4>
                 Price: &pound145.00;
             </h4>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#039;s standard dummy text ever since&hellip;</p>
-            <p><a href="#" class="add_to_basket" rel="31_1">Add to basket</a> </p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry&#039;s standard dummy text ever since&hellip;</p>
+            <p><a href="#" class="add_to_basket" rel="31_1">Add to basket</a></p>
         </div>
     </div>
     <div class="catalogue_wrapper">
         <div class="catalogue_wrapper_left">
 
             <a href="/?page=catalogue-item&amp;category=3&amp;id=31">
-                <img src="media/catalogue/unavailable.png" alt="Drum" width="120" />
+                <img src="media/catalogue/unavailable.png" alt="Drum" width="120"/>
             </a>
         </div>
         <div class="catalogue_wrapper_right">
             <h4>
                 <a href="/?page=catalogue-item&amp;category=3&amp;id=31"></a>
-                Drum                        </h4>
+                Drum </h4>
             <h4>
                 Price: &pound145.00;
             </h4>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#039;s standard dummy text ever since&hellip;</p>
-            <p><a href="#" class="add_to_basket" rel="31_1">Add to basket</a> </p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry&#039;s standard dummy text ever since&hellip;</p>
+            <p><a href="#" class="add_to_basket" rel="31_1">Add to basket</a></p>
         </div>
     </div>
     <div class="catalogue_wrapper">
         <div class="catalogue_wrapper_left">
 
             <a href="/?page=catalogue-item&amp;category=3&amp;id=31">
-                <img src="media/catalogue/unavailable.png" alt="Drum" width="120" />
+                <img src="media/catalogue/unavailable.png" alt="Drum" width="120"/>
             </a>
         </div>
         <div class="catalogue_wrapper_right">
             <h4>
                 <a href="/?page=catalogue-item&amp;category=3&amp;id=31"></a>
-                Drum                        </h4>
+                Drum </h4>
             <h4>
                 Price: &pound145.00;
             </h4>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#039;s standard dummy text ever since&hellip;</p>
-            <p><a href="#" class="add_to_basket" rel="31_1">Add to basket</a> </p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry&#039;s standard dummy text ever since&hellip;</p>
+            <p><a href="#" class="add_to_basket" rel="31_1">Add to basket</a></p>
         </div>
     </div>
 
 
-    <ul class="paging"><li><span>First</span></li><li><span>Previous</span></li><li><a href="/?page=catalogue&category=3&amp;pg=2">Next</a></li><li><span>Last</span><li></ul>
+    <ul class="paging">
+        <li><span>First</span></li>
+        <li><span>Previous</span></li>
+        <li><a href="/?page=catalogue&category=3&amp;pg=2">Next</a></li>
+        <li><span>Last</span>
+        <li>
+    </ul>
 
     </div>
     <div class="cl">&#160;</div>
 
 
-<?php require_once ("_footer.php"); ?>
+<?php require_once("_footer.php"); ?>
