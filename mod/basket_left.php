343a1cc87d2434ae8f53d3bce24f5a2665f3b42d
felerminoali <felerminoali@gmail.com>
2016-12-08 00:56:42 +0800

    First commit

--- mod/basket_left.php
+++ mod/basket_left.php
@@ -9,6 +9,40 @@
 <!--            --><?php //echo $objBasket->_number_of_items; ?>
         </span>
     </dd>
+
+    <dt>Books:</dt>
+    <dd class="bl_st">&pound;
+        <span>
+            1
+<!--            --><?php //echo number_format($objBasket->_sub_total, 2); ?>
+        </span>
+    </dd>
+
+    <dt>Audio:</dt>
+    <dd class="bl_st">&pound;
+        <span>
+            2
+            <!--            --><?php //echo number_format($objBasket->_sub_total, 2); ?>
+        </span>
+    </dd>
+
+    <dt>Videos:</dt>
+    <dd class="bl_st">&pound;
+        <span>
+            3
+            <!--            --><?php //echo number_format($objBasket->_sub_total, 2); ?>
+        </span>
+    </dd>
+
+
+    <dt>Periodicals:</dt>
+    <dd class="bl_st">&pound;
+        <span>
+            4
+            <!--            --><?php //echo number_format($objBasket->_sub_total, 2); ?>
+        </span>
+    </dd>
+    
 </dl>
 <div class="dev br_td">&#160;</div>
 <p><a href="/?page=basket">View </a> | <a href="/?page=checkout">Reserve now</a></p>
