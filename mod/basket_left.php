40c4f89884f50d6d09800dc98a2a0a19254c4016
felerminoali <felerminoali@gmail.com>
2016-12-08 15:30:33 +0800

    First commit

--- mod/basket_left.php
+++ mod/basket_left.php
@@ -4,16 +4,16 @@
 
 <dl id="basket_left">
     <dt>No. of items:</dt>
-    <dt class="bl_ti">
+    <dd class="bl_ti">
         <span>
             <?php echo $objBasket->_number_of_items; ?>
         </span>
-    </dt>
-    <dt class="bl_s max">
+    </dd>
+    <dd class="bl_s maximum">
         <span>
             <?php echo $objBasket->_summary; ?>
         </span>
-    </dt>
+    </dd>
 </dl>
 <div class="dev br_td">&#160;</div>
 <p><a href="/?page=basket">View </a> | <a href="/?page=checkout">Reserve now</a></p>
