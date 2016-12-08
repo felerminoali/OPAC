b5ed0d38aa882f90d442d1af8930170f0ec4b3d1
felerminoali <felerminoali@gmail.com>
2016-12-08 17:42:58 +0800

    First commit

--- pages/catalogue-item.php
+++ pages/catalogue-item.php
@@ -1,5 +1,7 @@
 <?php
 
+Login::restrictFront();
+
 $id = Url::getParam('id');
 
 if (!empty($id)) {
@@ -10,36 +12,72 @@ if (!empty($id)) {
     if (!empty($item)) {
         $category = $objCatalogue->getCategory($item['category']);
 
-        require_once ('_header.php');
-
-        echo '<h1>Catalogue :: ' . $category['name'] . '</h1>';
-
-        $image = !empty($item['image']) ? $objCatalogue->_path . $item['image'] : $objCatalogue->_path_alt . $category['image'];
-
-        if (!empty($image)) {
-            $width = Helper::getImgSize($image, 0);
-            $width = $width > 120 ? 120 : $width;
-            echo "<div class=\"fl_l\">";
-            echo "<div class=\"lft\"><img src=\"{$image}\" alt=\"";
-            echo Helper::encodeHTML($category['name'], 1);
-            echo "\" width=\"{$width}\" /></div>";
-        }
-
-        echo "<div class=\"rgt\"><h3>" . $item['title'] . "</h3>";
-//        echo "<h4><strong>&pound;" . $product['price'] . "</strong></h4>";
-        echo Basket::activeButton($item['id']);
-//        echo "</div></div>";
-//        echo "<div class=\"dev\">&#160;</div>";
-//        echo "<p>" . Helper::encodeHTML($product['description']) . "</p>";
-//        echo "<div class=\"dev br_td\">&#160;</div>";
-        echo "<p><a href=\"javascript:history.go(-1)\">Go back</a></p>";
+        require_once('_header.php');
+        ?>
 
 
+        <h1>Catalogue :: <?php echo $category['name']; ?></php></h1>;
 
-        require_once ('_footer.php');
+
+        <div class="fl_l">
+
+            <?php
+            $image = !empty($item['image']) ? $objCatalogue->_path . $item['image'] : $objCatalogue->_path_alt . $category['image'];
+
+
+            if (!empty($image)) {
+                $width = Helper::getImgSize($image, 0);
+                $width = $width > 120 ? 120 : $width;
+                ?>
+
+                <div class="lft">
+                    <img src="<?php echo $image; ?>" alt="<?php echo Helper::encodeHTML($category['name'], 1); ?>"
+                         width="<?php echo $width; ?>"/>
+                </div>
+
+                <?php
+            } ?>
+
+            <div class="rgt">
+                <h3>
+                    <strong>
+                        <?php echo $item['title']; ?>
+                    </strong>
+                </h3>
+
+
+                <?php if (!empty($item['author'])) { ?>
+                    <p>
+                        by: <u><?php echo $item['author']; ?></u>;
+                    </p>
+                <?php } ?>
+
+                <?php if (!empty($item['year'])) { ?>
+                    <p>
+                        <?php echo $item['year']; ?>;
+                    </p>
+                <?php } ?>
+
+                <?php if (!empty($item['publishing_date'])) { ?>
+                    <p>
+                        <?php echo $item['publishing_date']; ?>;
+                    </p>
+                <?php } ?>
+
+                <?php echo Basket::activeButton($item['id']); ?>
+
+            </div>
+        </div>
+        <div class="dev">&#160;</div>
+        <p><?php echo Helper::encodeHTML($product['description']); ?></p>
+        <div class="dev br_td">&#160;</div>
+        <p><a href="javascript:history.go(-1)">Go back</a></p>
+
+        <?php
+        require_once('_footer.php');
     } else {
-        require_once ('error.php');
+        require_once('error.php');
     }
 } else {
-    require_once ('error.php');
+    require_once('error.php');
 }
