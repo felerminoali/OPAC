c8d5c5a574d23f0b990b55317644757c6d18f8f6
felerminoali <felerminoali@gmail.com>
2016-12-08 14:49:21 +0800

    First commit

--- mod/basket_small_refresh.php
+++ mod/basket_small_refresh.php
@@ -3,5 +3,5 @@ require_once("../inc/autoload.php");
 $objBasket = new Basket();
 $out = array();
 $out['bl_ti'] = $objBasket->_number_of_items;
-$out['bl_s'] = $objBasket->_summary;
+$out['bl_s'] = Helper::encodeHTML($objBasket->_summary);
 echo json_encode($out);
