264b483817d0a87d4d0bea435bfe0ab78499db48
felerminoali <felerminoali@gmail.com>
2016-12-08 15:58:56 +0800

    First commit

---
+++ pages/basket.php
@@ -0,0 +1,61 @@
+<?php
+$session = Session::getSession('basket');
+
+$objBasket = new Basket();
+
+$out = array();
+
+if (!empty($session)) {
+    $objCatalogue = new Catalogue();
+
+    foreach ($session as $key => $value) {
+        $out[$key] = $objCatalogue->getItem($key);
+    }
+}
+
+require_once ('_header.php');
+?>
+
+<h1>My Basket</h1>
+
+<?php if (!empty($out)) { ?>
+    <div id="big_basket">
+
+        <form action="" method="POST" id="frm_basket">
+            <table cellpadding="0" cellspacing="0" border="0" class="tbl_repeat">
+                <tr>
+                    <th>Item</th>
+<!--                    <th class="ta_r">Qty</th>-->
+<!--                    <th class="ta_r col_15">Price</th>-->
+                    <th class="ta_r col_15">Remove</th>
+                </tr>
+
+                <?php foreach ($out as $item) { ?>
+                    <tr>
+                        <td><?php echo Helper::encodeHTML($item['title']); ?></td>
+<!--                        <td>-->
+<!--                            <input type="text" name="qty---><?php //echo $item['id']; ?><!--"-->
+<!--                                   id="qty---><?php //echo $item['id']; ?><!--" class="fld_qty"-->
+<!--                                   value="--><?php //echo $session[$item['id']]['qty']; ?><!--" />-->
+<!--                        </td>-->
+<!--                        <td class="ta_r">&pound;--><?php //echo number_format($objBasket->itemTotal($item['price'], $session[$item['id']]['qty']), 2); ?><!--</td>-->
+                        <td class="ta_r"><?php echo Basket::removeButton($item['id']); ?></td>
+                    </tr>
+
+                <?php } ?>
+
+                </table>
+
+                <div class="dev br_td">&#160;</div>
+                <div class="sbm sbm_blue fl_r">
+                    <a href="/?page=checkout" class="btn">Reverve Now</a> 
+                </div>
+
+
+            </form>
+        </div>
+    
+} else { ?>
+    <p>Your basket is currently empty</p>
+<?php } ?>
+<?php require_once ('_footer.php'); ?>
