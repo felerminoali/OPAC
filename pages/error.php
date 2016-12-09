a0a268c49e3dfb0b6d0b1bf488f5a3a80594846e
felerminoali <felerminoali@gmail.com>
2016-12-09 21:51:39 +0800

    First commit

--- pages/error.php
+++ pages/error.php
@@ -5,3 +5,17 @@
  * Date: 12/4/2016
  * Time: 5:16 PM
  */
+
+require_once("_header.php"); ?>
+
+<h1>ERROR</h1>
+
+<p>Something went wrong</p>
+
+<p>Please contact Administrator</p>
+
+<p>Click the link below to go to home page:</p>
+<a href="/">Go Home</a>
+
+
+<?php require_once("_footer.php"); ?>
