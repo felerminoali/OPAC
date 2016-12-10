c6c23ce15677ea464e79cf4782d3e883c99507a8
felerminoali <felerminoali@gmail.com>
2016-12-10 17:02:54 +0800

    First commit

--- mod/renew_loan.php
+++ mod/renew_loan.php
@@ -3,21 +3,25 @@ require_once ('../inc/autoload.php');
 
 if(isset($_POST['loans'])){
 
-
-//    $array = array();
-
-    $loans = $_POST['loans'];
-
-    $out = '';
-    foreach ($loans as $loan) {
-        $l = explode("_",$loan);
-
-        $out .= ' | id: '.$l[0];
-        $out .= ' item: '.$l[1];
-        $out .= ' reservation: '.$l[2];
+    $loans_param_array = $_POST['loans'];
+    
+    $loans = array();
+    
+    foreach ($loans_param_array as $loan_element) {
+        $l = explode("_",$loan_element);
+        
+        $loans[] = $l; 
         $l = null;
     }
 
+    $out = '';
+    foreach ($loans as $loan){
+
+        $out .= ' | id: '.$loan[0];
+        $out .= ' item: '.$loan[1];
+        $out .= ' reservation: '.$loan[2];
+    }
+    
 
 
 
