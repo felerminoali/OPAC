e44960abfe04bbbf74a7094f9d3f216e8327ae37
felerminoali <felerminoali@gmail.com>
2016-12-11 03:05:07 +0800

    First commit

--- mod/submit_comment.php
+++ mod/submit_comment.php
@@ -14,7 +14,9 @@ if(isset($_POST['reservation']) && $_POST['comment']){
     $array = array();
     $array['reservation'] = $_POST['reservation'];
     $array['comment'] = $_POST['comment'];
-    $array['date_posted'] = Helper::setDate();
+
+    $now = new DateTime();
+    $array['date_posted'] = $now->format('Y-m-d-H:i:s');
     
     $objReservation = new Reservation();
     $objReservation->addFeedBackComments($array);
