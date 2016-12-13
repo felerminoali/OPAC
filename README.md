e88c73dfb9b3db23c2083ed64a4f117319047d53
felerminoali <felerminoali@gmail.com>
2016-12-14 00:14:56 +0800

    First commit

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 
 Myopac
-
+====================
 
 login: guest
 password: password
 
 OR
 
-you can register
+you can register by providing a valid email
+
+
+After login into the system you can perform the following functionalities:
+
+Catalogue - Search catalogue and place reservation
+My account - View and edit your personal details.
+My loans  - View your loans and renew.
+My reservations - View the status of any reservation that you have and provide feedback.
+
+
 
 ------------------------------------
 
@@ -16,6 +26,8 @@ LMS: http://myopac.herokuapp.com/library/
 
 login: admin
 password: password
+
+Check out - check out items
 
 -------------------------------------
 
--- pages/index.php
+++ pages/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once ('_header.php')
+require_once('_header.php')
 ?>
 
 
@@ -7,17 +7,39 @@ require_once ('_header.php')
     <div class='section friendly'>
         <h1><strong>OPAC | Welcome to your new app!</strong></h1>
         <div class='article'>
-            <p>Author: Felermino Ali</p>
+            <p>Developer: Felermino Ali</p>
             <p>What is Lorem Ipsum?</p>
-            <p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            <p><strong>Lorem Ipsum</strong> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+                architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur
+                aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
+                porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non
+                numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad
+                minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
+                commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
+                molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
             </p>
             <p>Refer to the <a href="https://myopac.herokuapp.com/?page=about">link</a> if you need more info.</p>
+            
+            <p>login: guest</p>
+            <p>password: password</p>
+            <p>OR</p>
+            <p>you can register by providing a valid email</p>
+            <p>After login into the system you can perform the following functionalities:
+<br/>
+                <strong>Catalogue</strong> - Search catalogue and place reservation
+                <br/>
+                <strong>My account</strong> - View and edit your personal details.
+                <br/>
+                <strong>My loans</strong>  - View your loans and renew.
+                <br/>
+                <strong>My reservations </strong>- View the status of any reservation that you have and provide feedback.</p>
         </div>
     </div>
 </div>
 
 
-<?php require_once ('_footer.php')?>
+<?php require_once('_footer.php') ?>
 
 
 
