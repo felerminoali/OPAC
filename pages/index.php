6ea3e9670cf1971cf1407165688d43c403fa1d2e
felerminoali <felerminoali@gmail.com>
2016-12-14 00:17:13 +0800

    First commit

--- pages/index.php
+++ pages/index.php
@@ -8,18 +8,7 @@ require_once('_header.php')
         <h1><strong>OPAC | Welcome to your new app!</strong></h1>
         <div class='article'>
             <p>Developer: Felermino Ali</p>
-            <p>What is Lorem Ipsum?</p>
-            <p><strong>Lorem Ipsum</strong> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-                doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-                architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur
-                aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
-                porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non
-                numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad
-                minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
-                commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
-                molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-            </p>
-            <p>Refer to the <a href="https://myopac.herokuapp.com/?page=about">link</a> if you need more info.</p>
+            
             
             <p>login: guest</p>
             <p>password: password</p>
@@ -34,6 +23,8 @@ require_once('_header.php')
                 <strong>My loans</strong>  - View your loans and renew.
                 <br/>
                 <strong>My reservations </strong>- View the status of any reservation that you have and provide feedback.</p>
+
+            <p>Refer to the <a href="https://myopac.herokuapp.com/?page=about">link</a> if you need more info.</p>
         </div>
     </div>
 </div>
